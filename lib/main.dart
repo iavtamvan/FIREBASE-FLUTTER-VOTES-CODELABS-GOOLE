2d77f794b0f85efc680d5dbe592de01eb0c940ed
iavtamvan <ade.fajr.ariav@gmail.com>
2019-04-06 17:01:47 +0700

    adding image

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Baby Names',
+      title: 'Baby Names <Nama Apps>',
       home: MyHomePage(),
     );
   }
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
+      appBar: AppBar(title: Text('Pemilihan Presiden')),
       body: _buildBody(context),
     );
   }
@@ -49,7 +49,26 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
+//    return MaterialApp(
+//      home: Scaffold(
+////        appBar: AppBar(title: Text('First app')),
+//        body: Row(
+//          children: <Widget>[
+//            Image.asset(
+//              'assets/bolah.jpg',
+//              width: 10,
+//              height: 10,
+//            ),
+////            ListView(
+////              children: snapshot
+////                  .map((data) => _buildListItem(context, data))
+////                  .toList(),
+////            )
+//          ],
+//        ),
+//      ),
+//    );
+    return new ListView(
       padding: const EdgeInsets.only(top: 20.0),
       children: snapshot.map((data) => _buildListItem(context, data)).toList(),
     );
@@ -57,39 +76,80 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
     final record = Record.fromSnapshot(data);
-
+//          onTap: () => Firestore.instance.runTransaction((transaction) async {
+//                final freshSnapshot = await transaction.get(record.reference);
+//                final fresh = Record.fromSnapshot(freshSnapshot);
+//
+//                await transaction
+//                    .update(record.reference, {'votes': fresh.votes + 1});
+//              }),
+//        ),
     return Padding(
       key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () => Firestore.instance.runTransaction((transaction) async {
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+      child: new ListTile(
+          onTap: () => Firestore.instance.runTransaction((transaction) async{
             final freshSnapshot = await transaction.get(record.reference);
             final fresh = Record.fromSnapshot(freshSnapshot);
+            await transaction.update(record.reference, {'votes': fresh.votes + 1});
+          }) ,
+          leading: new CircleAvatar(
+            backgroundColor: Colors.transparent,
+//            child: new Image(image: new AssetImage('assets/bolah.jpg')),
+            child: new Image.network(record.image, width: 800, height: 200),
+          ),
+          title: new Row(
+            children: <Widget>[
+              new Title(color: Colors.red, child: Expanded(child: new Text(record.name),)),
+//              new Text(record.name),
+//              new Expanded(child: new Text(record.name)),
+              new Expanded(child: new Text(record.votes.toString())),
 
-            await transaction
-                .update(record.reference, {'votes': fresh.votes + 1});
-          }),
-        ),
-      ),
+//              new Checkbox(value: product.isCheck, onChanged: (bool value) {
+//                setState(() {
+//                  product.isCheck = value;
+//                });
+//              })
+            ],
+          )
+      )
     );
+
+
+//      Container(
+//        decoration: BoxDecoration(
+//          border: Border.all(color: Colors.grey),
+//          borderRadius: BorderRadius.circular(5.0),
+//        ),
+//
+//        child: ListTile(
+//          Image : new Image(image: new AssetImage('assets/bolah.jpg')),
+//          title: Text(record.name),
+////          subtitle: Text(record.image),
+//          trailing: Text(record.votes.toString()),
+//          onTap: () => Firestore.instance.runTransaction((transaction) async {
+//                final freshSnapshot = await transaction.get(record.reference);
+//                final fresh = Record.fromSnapshot(freshSnapshot);
+//
+//                await transaction
+//                    .update(record.reference, {'votes': fresh.votes + 1});
+//              }),
+//        ),
+//      ),
   }
 }
 
 class Record {
+  final String image;
   final String name;
   final int votes;
   final DocumentReference reference;
 
   Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['name'] != null),
+      : assert(map['image'] != null),
+        assert(map['name'] != null),
         assert(map['votes'] != null),
+        image = map['image'],
         name = map['name'],
         votes = map['votes'];
 
@@ -97,5 +157,5 @@ class Record {
       : this.fromMap(snapshot.data, reference: snapshot.reference);
 
   @override
-  String toString() => "Record<$name:$votes>";
+  String toString() => "Record<$image:$name:$votes>";
 }
